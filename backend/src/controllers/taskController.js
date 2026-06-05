d06abf2466a18f116a9c7e3003b25cd94bb5be6b
Prithviraj383 <prithvichidri5@gmail.com>
2026-06-05 21:46:20 +0530

    implement getTasks api

--- backend/src/controllers/taskController.js
+++ backend/src/controllers/taskController.js
@@ -11,9 +11,32 @@ const createTask = asyncHandler(async (req, res) => {
         return res.status(403).json({ message: "You are not a member of this board" })
     }
 
+    const check2 = await db.query("SELECT * FROM tasks WHERE title = $1 AND board_id = $2", [title, boardId])
+    if (check2.rows.length > 0) {
+        return res.status(400).json({ message: "Task with this title already exists in the board" })
+    }
 
     const result = await db.query("INSERT INTO tasks (title, description, board_id, created_by) VALUES ($1, $2, $3, $4)", [title, description, boardId, userId])
     res.status(201).json({message: "Task created successfully", task: result.rows[0]})
 })
 
-export {createTask}
+const getTasks = asyncHandler(async (req, res) => {
+    const { boardId } = req.params
+
+    const userId = req.user.id
+    console.log(boardId, userId)
+    const check = await db.query("SELECT * FROM boards WHERE id = $1 AND created_by = $2", [boardId, userId])
+    if (check.rows.length === 0) {
+        return res.status(403).json({ message: "You are not a member of this board" })
+    }
+
+    const check2 = await db.query("SELECT * FROM tasks WHERE board_id = $1", [boardId])
+    if (check2.rows.length === 0) {
+        return res.status(404).json({ message: "No tasks found for this board" })
+    }
+
+    const result = await db.query("SELECT * FROM tasks WHERE board_id = $1", [boardId])
+    res.status(200).json({ tasks: result.rows })
+})
+
+export {createTask, getTasks}
--- backend/src/routes/taskRoutes.js
+++ backend/src/routes/taskRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express'
 import {authMiddleware} from '../middlewares/authMiddleware.js'
-import {createTask} from '../controllers/taskController.js'
+import {createTask, getTasks} from '../controllers/taskController.js'
 
 const router = express.Router({ mergeParams: true })
 
 router.post("/", authMiddleware, createTask)
+router.get("/", authMiddleware, getTasks)
 
 export default router
