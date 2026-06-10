a5037afbe3d4d1251be465634f1b363a032d1b5e
Prithviraj383 <prithvichidri5@gmail.com>
2026-06-10 19:32:40 +0530

    added remove_member function

--- backend/src/controllers/workspaceMemberController.js
+++ backend/src/controllers/workspaceMemberController.js
@@ -75,4 +75,52 @@ const getMembers = asyncHandler(async (req, res) => {
   });
 });
 
-export { inviteMember, getMembers };
+const removeMember = asyncHandler(async (req, res) => {
+  const { workspaceId } = req.params;
+  const { email } = req.body;
+  const userId = req.user.id;
+
+  //workspace check
+  const wCheck = await db.query("SELECT * FROM workspaces WHERE id = $1", [
+    workspaceId,
+  ]);
+  if (wCheck.rows.length == 0) {
+    return res.status(400).json({ message: "WorkSpace doesnt exist" });
+  }
+
+  //check if logged in user is the owner of the workspace
+  if (wCheck.rows[0].owner_id != userId) {
+    return res
+      .status(403)
+      .json({ message: "You arent the owner of the workspace" });
+  }
+
+  //check if user exists
+  const userCheck = await db.query("SELECT * FROM users WHERE email = $1", [
+    email,
+  ]);
+  if (userCheck.rows.length == 0) {
+    return res.status(400).json({ message: "User doesnt exist" });
+  }
+
+  const memberUserId = userCheck.rows[0].id;
+
+  //check if the member already exist
+  const check = await db.query(
+    "SELECT * FROM workspace_members WHERE workspace_id = $1 AND user_id = $2",
+    [workspaceId, memberUserId],
+  );
+  if (check.rows.length == 0) {
+    return res.status(400).json({ message: "User is not a member of the workspace" });
+  }
+
+  //remove the member
+  await db.query("DELETE FROM workspace_members WHERE workspace_id = $1 AND user_id = $2", [
+    workspaceId,
+    memberUserId,
+  ]);
+
+  return res.status(200).json({ message: "Member removed successfully" });
+});
+
+export { inviteMember, getMembers, removeMember };
--- backend/src/routes/workspaceMemberRoutes.js
+++ backend/src/routes/workspaceMemberRoutes.js
@@ -1,4 +1,4 @@
-import {inviteMember, getMembers} from '../controllers/workspaceMemberController.js'
+import {inviteMember, getMembers, removeMember} from '../controllers/workspaceMemberController.js'
 import express from 'express'
 import {authMiddleware} from '../middlewares/authMiddleware.js'
 
@@ -6,4 +6,6 @@ const router = express.Router({mergeParams: true})
 
 router.post("/", authMiddleware, inviteMember)
 router.get("/", authMiddleware, getMembers)
+router.delete("/", authMiddleware, removeMember)
+
 export default router
